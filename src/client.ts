c32958b35af2fa1af558bc2e553ab3d84f1c37d7
Eric Kinolik <ekinolik@gmail.com>
2024-12-28 12:45:25 -0800

    remove client side caching

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,6 @@ import { GetDexRequest, GetDexResponse, GetDexByStrikesRequest } from './generat
 import { GetLastTradeRequest, GetLastTradeResponse } from './generated/market/v1/market_pb';
 import { DexServiceClient } from './generated/dex/v1/dex_grpc_pb';
 import { MarketServiceClient } from './generated/market/v1/market_grpc_pb';
-import { DexCache } from './cache/dexCache';
 
 export interface ClientOptions {
   host: string;
@@ -27,12 +26,10 @@ export interface GetDexByStrikesParams {
 export class JaxClient {
   private dexClient: InstanceType<typeof DexServiceClient>;
   private marketClient: InstanceType<typeof MarketServiceClient>;
-  private dexCache: DexCache;
 
   constructor(options: ClientOptions) {
     this.dexClient = new DexServiceClient(options.host, grpc.credentials.createInsecure());
     this.marketClient = new MarketServiceClient(options.host, grpc.credentials.createInsecure());
-    this.dexCache = new DexCache();
   }
 
   async getDex(params: GetDexParams): Promise<GetDexResponse> {
@@ -46,43 +43,11 @@ export class JaxClient {
       request.setEndStrikePrice(params.endStrikePrice);
     }
 
-    // Check cache first with strike range
-    const cachedResponse = this.dexCache.get(
-      params.underlyingAsset, 
-      params.startStrikePrice, 
-      params.endStrikePrice
-    );
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-
-    // If not in cache, make the API call
     return new Promise((resolve, reject) => {
-      this.dexClient.getDex(request, {}, (err, response, details) => {
+      this.dexClient.getDex(request, {}, (err, response) => {
         if (err) {
           reject(err);
           return;
-        }
-        
-        // Update cache with strike range
-        const expiresAt = details?.metadata?.get('cache-expires-at')?.[0];
-        if (expiresAt) {
-          const ttl = (parseInt(expiresAt) * 1000) - Date.now();
-          this.dexCache.set(
-            params.underlyingAsset, 
-            response, 
-            ttl, 
-            params.startStrikePrice, 
-            params.endStrikePrice
-          );
-        } else {
-          this.dexCache.set(
-            params.underlyingAsset, 
-            response, 
-            undefined, 
-            params.startStrikePrice, 
-            params.endStrikePrice
-          );
         }
         resolve(response);
       });
@@ -109,27 +74,11 @@ export class JaxClient {
     request.setUnderlyingAsset(params.underlyingAsset);
     request.setNumStrikes(params.numStrikes);
 
-    // Check cache first
-    const cachedResponse = this.dexCache.get(params.underlyingAsset);
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-
-    // If not in cache, make the API call
     return new Promise((resolve, reject) => {
-      this.dexClient.getDexByStrikes(request, {}, (err, response, details) => {
+      this.dexClient.getDexByStrikes(request, {}, (err, response) => {
         if (err) {
           reject(err);
           return;
-        }
-        
-        // Update cache
-        const expiresAt = details?.metadata?.get('cache-expires-at')?.[0];
-        if (expiresAt) {
-          const ttl = (parseInt(expiresAt) * 1000) - Date.now();
-          this.dexCache.set(params.underlyingAsset, response, ttl);
-        } else {
-          this.dexCache.set(params.underlyingAsset, response);
         }
         resolve(response);
       });
